36d9f47b209823c4fe609cdb914924949b968877
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-02 21:48:33 +0000

    mobile up the nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,69 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 
 import NavLink from "../components/nav-link"
 import Logo from "../svg/logo.svg"
 
-const Header = () => (
-  <header className="bg-secondary font-display flex" style={{ height: `70px` }}>
-    <div className="w-2/3 mx-auto flex items-center justify-between">
-      <Link to="/">
-        <Logo />
-      </Link>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/facilities/">Facilities</NavLink>
-        <NavLink to="/book/">Book</NavLink>
-        <NavLink to="/contact-us/">Contact</NavLink>
-      </nav>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [isExpanded, toggleExpansion] = useState(false)
+  const pages = [
+    {
+      route: `/`,
+      text: `Home`
+    },
+    {
+      route: `/facilities`,
+      text: `Facilities`
+    },
+    {
+      route: `/book`,
+      text: `Book`
+    },
+    {
+      route: `/contact-us`,
+      text: `Contact Us`
+    }
+  ]
+  return (
+    <header
+      className="bg-secondary font-display flex"
+      style={{ minHeight: `70px` }}
+    >
+      <div className="w-2/3 mx-auto p-4 flex flex-wrap items-center justify-between">
+        <Link to="/">
+          <Logo />
+        </Link>
+        <nav className="hidden md:block">
+          {pages.map(page => (
+            <NavLink key={page.text} to={page.route}>
+              {page.text}
+            </NavLink>
+          ))}
+        </nav>
+        <button
+          className="md:hidden text-white border-white border-2 rounded p-2"
+          onClick={() => toggleExpansion(!isExpanded)}
+        >
+          Menu
+        </button>
+        <nav
+          className={`${
+            isExpanded ? `block` : `hidden`
+          } md:hidden md:flex md:items-center w-full md:w-auto`}
+        >
+          {pages.map(page => (
+            <Link
+              className="block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-white"
+              key={page.text}
+              to={page.route}
+            >
+              {page.text}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
+}
 
 export default Header
--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const NavLink = ({ to, children }) => (
   <Link
-    className="text-white font-display mr-10 ml-10 leading-loose border-b-2 border-transparent hover:border-white hover:no-underline"
+    className="text-white font-display mr-2 ml-2 lg:ml-10 lg:mr-10 leading-loose border-b-2 border-transparent hover:border-white hover:no-underline"
     activeStyle={{ borderBottom: `2px solid #EE6C4D` }}
     to={to}
   >
