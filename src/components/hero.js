bfc3f8ec3dce5dc174bb77debb5731636fe257e1
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-02 22:04:08 +0000

    fix spacing and breakpoints

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,16 +4,16 @@ import Button from "../components/button"
 
 const Hero = ({ text, image, cta }) => (
   <section className="bg-primary" style={{ minHeight: `400px` }}>
-    <div className="w-4/5 md:w-2/3 mx-auto flex flex-wrap justify-between pt-6 md:pt-16">
-      <div>
-        <h2 className="w-full md:w-1/2 text-white font-bold text-xl md:text-2xl mr-20 font-body mt-0">
+    <div className="w-4/5 md:w-2/3 mx-auto flex flex-wrap md:justify-between pt-6 md:pt-16">
+      <div className="w-full md:w-1/2">
+        <h2 className="text-white font-bold text-xl md:text-2xl md:mr-20 font-body mt-0">
           {text}
         </h2>
         {cta && <Button to={cta.to}>{cta.text}</Button>}
       </div>
       <div
-        className="w-full md:w-1/2 items-center"
-        style={{ height: `300px`, width: `300px` }}
+        className="w-full md:w-1/2"
+        style={{ minHeight: `300px`, minWidth: `300px` }}
       >
         {image}
       </div>
