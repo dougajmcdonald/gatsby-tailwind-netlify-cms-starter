834f130b6023c24e717c250307a88e079774b70e
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-02 11:28:45 +0000

    add logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,14 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+import Logo from "../svg/logo.svg"
+
+const Header = () => (
   <header className="bg-secondary font-display flex" style={{ height: `70px` }}>
-    <div className="w-2/3 mx-auto">
-      <h1 className="text-white">
-        <Link to="/">{siteTitle}</Link>
-      </h1>
+    <div className="w-2/3 mx-auto flex items-center">
+      <Logo />
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string
-}
-
-Header.defaultProps = {
-  siteTitle: ``
-}
 
 export default Header
